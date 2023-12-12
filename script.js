b650651855c4ff1a4538645c16d372b4f03674cb
pswanagan <pswanagan@gmail.com>
2023-12-12 15:33:35 -0600

    added template and frag

--- script.js
+++ script.js
@@ -32,6 +32,9 @@ document.addEventListener('DOMContentLoaded', function () {
         const trait = traitInput.value.trim();
         const selectedColor = colorSelect.value;
 
+    console.log(ingVerb);
+
+
         // Create a story using user inputs
         const story = `Once upon a time, in a ${adjective} forest, there lived a young adventurer named ${charName}. ${charName} had always dreamed of ${ingVerb} the world beyond the trees. One sunny morning, they decided it was time to embark on a grand ${noun}.
 
@@ -39,7 +42,36 @@ With a heart full of ${trait}, ${charName} set off into the forest. The towering
 
 The forest had granted ${charName}'s deepest wish, and their adventure had only just begun.`;
 
-        // Display the story in the story box
+        
+
+        // Create a new image element
+    const randomImage = document.createElement('img');
+    randomImage.src = getRandomImageUrl();
+    randomImage.alt = 'Random Reward Image';
+
+    // Append the new image next to the forest image
+    const forestImageParent = document.getElementById('forestImage').parentNode;
+    forestImageParent.appendChild(randomImage);
+
+    // Select and clone the template
+    const template = document.querySelector('#the-end').content;
+    const clone = template.cloneNode(true);
+
+    // Create a DocumentFragment
+    const fragment = document.createDocumentFragment();
+
+    // Append the cloned content to the fragment
+    fragment.appendChild(clone);
+
+    // Append the fragment to the desired location in the document
+    const appendToElement = document.querySelector("#box-story");
+    appendToElement.appendChild(fragment);
+
+
+        // Add a border around the story in story-box
+        storyBox.style.border = "2px solid black";
+        
+        // Display the story in the story-box
         storyBox.textContent = story;
 
         // Change the background color based on user selection
@@ -47,7 +79,15 @@ The forest had granted ${charName}'s deepest wish, and their adventure had only 
 
         // Alert box to show the story has been successfully created!
         window.alert("Story created successfully!");
+        // Alert box to show that the image appended behind the forest image is their reward.
+        window.alert("Here's your reward!!!");
     });
+
+    // Function to generate a random image URL
+function getRandomImageUrl() {
+    const imageUrls = ['crown.jpg', 'book.jpg', 'sword.jpg']; 
+    return imageUrls[Math.floor(Math.random() * imageUrls.length)];
+}
     // Handle input validation for Character Name input
     charNameInput.addEventListener('input', function () {
         if(charNameInput.value.length < 3) {
@@ -56,11 +96,33 @@ The forest had granted ${charName}'s deepest wish, and their adventure had only 
             charNameInput.setCustomValidity("");
         }
     });
-
+    ingVerbInput.addEventListener('input', function() {
+        if (!/ing$/i.test(ingVerbInput.value.trim())) {
+            ingVerbInput.setCustomValidity("Please enter a verb ending in 'ing'");
+        } else {
+            ingVerbInput.setCustomValidity("");
+        }
+    });
     // Handle form reset
     form.addEventListener('reset', function () {
         // Clear the story and reset the background color
         document.querySelectorAll('input[type="text"]').forEach(input => input.value = '');
+         // Select the parent element of the forest image
+    const forestImageParent = document.getElementById('forestImage').parentNode;
+
+    // Remove all appended images except the forest image
+    const images = forestImageParent.querySelectorAll('img');
+    images.forEach(img => {
+        if (img.id !== 'forestImage') {
+            forestImageParent.removeChild(img);
+        }
+    });
+    const boxStory = document.getElementById('box-story');
+    const addedTemps = boxStory.getElementsByTagName('h1');
+    Array.from(addedTemps).forEach(item => {
+        boxStory.removeChild(item);
+    });
+        storyBox.style.border = '';
         storyBox.textContent = '';
         document.body.style.backgroundColor = '';
     });
