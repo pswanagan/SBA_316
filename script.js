5dcdd9f1cc96113b22f8361d02dad4214bb6cf06
pswanagan <pswanagan@gmail.com>
2023-12-13 12:41:57 -0600

    deleted unnecessary comments

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-/*const forestStory = `Once upon a time, in a mystical forest, there lived a courageous adventurer named ${name}. ${name} was known far and wide for their extraordinary ${trait}. One day, while ${ing} through the woods, they stumbled upon a hidden ${noun}, which emitted a strange ${adj} glow.
-
-Curiosity piqued, ${name} approached the mysterious ${noun}. As they reached out to touch it, the forest came alive with a soft, melodic hum. Suddenly, a ${adj} portal appeared before them, leading to a world filled with ${pNoun}.
-
-Without hesitation, ${name} stepped through the portal, embarking on a remarkable adventure. Along the way, they encountered ${noun}, befriended ${pNoun}, and solved puzzles using their ${trait}.
-
-With each challenge they faced, ${name} grew wiser and more ${trait}. After a series of thrilling escapades, they discovered a hidden treasure chest filled with ${pNoun}. It was a reward well-deserved.
-
-With their newfound treasures, ${name} returned to their world, forever cherishing the memories of their enchanting forest quest.`*/
+// Javascript 
 document.addEventListener('DOMContentLoaded', function () {
     // Get references to form elements and story box
     const charNameInput = document.getElementById('charName');
